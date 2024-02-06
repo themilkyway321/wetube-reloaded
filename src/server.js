c517af751ab293add56594ce6ddd1ae99bfca31e
themilkyway321 <alfl05@hanmail.net>
2024-02-06 21:45:37 +0900

    20240206

--- src/server.js
+++ src/server.js
@@ -1,21 +1,25 @@
 import express from "express";
 
-// express 를 실현하는 어플리케이션
+// express 를 실현하는 app 
 const app = express();
 const PORT = 4000;
-const handleListening = () =>
-  console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
+const handleListening = () => console.log(`✅ Server listenting on port http://localhost:${PORT} 🚀`);
 
-const handleHome = (req, res)=> {
-    return res.send("hi miri");
-};
+
 const handleLogin = (req, res)=> {
     return res.send("login here");
 };
 //express 앱 서버가 들을 수 있도록
 
-
-app.get("/", handleHome);
+const logger = (req, res, next) =>{
+  console.log(`${req.method}, ${req.url}`);
+  next();
+}
+const handleHome = (req, res)=> {
+  console.log("how about this?")
+  return res.send("hi miri");
+};
+app.get("/", logger, handleHome);
 app.get("/login", handleLogin);
 app.listen(PORT, handleListening);
 
